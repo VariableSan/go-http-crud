e23d77de0361a9e4fbd375058c411143553e8a3b
VariableSan <nursultan_nurgaliyev@proton.me>
2025-07-31 14:08:46 +0500

    Refactor database connection handling and update application structure

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,28 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/VariableSan/go-http-crud/internal/api"
+	"github.com/VariableSan/go-http-crud/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func New() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
@@ -24,6 +32,7 @@ func New() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
