05a5052ebe0fe18c29d01884d234e159ec4f9613
VariableSan <nursultan_nurgaliyev@proton.me>
2025-08-08 13:35:05 +0500

    Implement token management functionality with TokenHandler and PostgresTokenStore, add token generation logic, and create migration for tokens table

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,13 +9,15 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/health", app.HealthCheck)
-	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
 
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
 	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
 	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
 
 	r.Post("/users", app.UserHandler.HandleRegisterUser)
+
+	r.Post("/tokens/authentication", app.TokenHandler.HandleCreateToken)
 
 	return r
 }
